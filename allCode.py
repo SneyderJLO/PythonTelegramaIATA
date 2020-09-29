b9444bc4e658f2115270b7c3509ddd11268324a4
SneyderJLO <tenemriq@outlook.com>
2020-09-28 22:31:20 -0500

    beautifilSoup

--- allCode.py
+++ allCode.py
@@ -3,30 +3,59 @@ import telebot
 from telebot import types
 import time
 import json
+from bs4 import BeautifulSoup
 import requests
 import telegram
 from telegram.ext import *
 TOKEN = '1275373802:AAGn8auWnyZWRjlDbO4zAD4446ThP5OSwbQ'
 URL = "https://api.telegram.org/bot" + TOKEN + "/"
-Menu = '¿Qué deseas hacer?: \n\n/Buscar  \n/info - Informacion De interes \n/hola - Saludo del Bot \n/piensa3D - Informacion sobre Piensa3D \n\n'
+Menu = '¿Qué deseas hacer?: \n\n/Comprar vuelo  \n/Info - Acerca de nosotros \n\n'
 
 bot = telegram.Bot(token = '1275373802:AAGn8auWnyZWRjlDbO4zAD4446ThP5OSwbQ')
+mibot = telebot.TeleBot(TOKEN)
 updater = Updater(TOKEN)
 botUpdater = updater
 
-def start(bot, update, pass_chat_data = True):
-    update.message.chat.id
-    bot.sendMessage(chat_id = update.message.chat.id, text = Menu)
 
+def datosAirlines():
+    sitioCodes = 'https://madavan.com.mx/codigo-iata-aerolineas/'
+    pagina = requests.get(URL)
+    soup = BeautifulSoup(pagina.content, 'html.parser')
+
+
+def start(bot, update, pass_chat_data = True):
+
+    miID = update.message.chat.id
+    c.append(str(miID))
+    bot.sendMessage(chat_id = miID, text = '¡Hola! ¡Te habla el PanaMiguel 😹 y soy un bot interactivo!'
+                                           '\nTe ayudaré a realizar correctamente la compra de tu boleto de vuelo'
+                                           '\nElige tu opción, dando click al comando o escribiéndolo')
+
+    bot.sendMessage(chat_id = miID, text = Menu)
+
+
+def comprar(bot, update):
+    miID = update.message.chat.id
+    bot.sendMessage(chat_id = miID, text = 'Aqui vas a comprar xd')
+
+
+
+listaIata = list()
+listaAirlines = list()
+listPaises = list()
+c =  list()
 start_handler = CommandHandler('start', start)
+comprar_handler = CommandHandler('comprar', comprar)
 dispatcher = botUpdater.dispatcher
 dispatcher.add_handler(start_handler)
 botUpdater.start_polling()
+dispatcher.add_handler(comprar_handler)
+
 botUpdater.idle()
+
 
 while True:
     pass
-
 
 
 
